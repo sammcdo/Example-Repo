b334845c73abe09ab2d3b5c06682e0b28df5b3c3
sammcdo <sam_mcdo@yahoo.com>
2021-09-04 20:25:52 -0700

    added some javadoc - sammcdo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,14 @@
+/**
+ * Print hello world to the user
+ * 
+ * @author sammcdo
+ */
 public class HelloWorld {
+  /**
+   * print hello world to the user
+   * 
+   * @param args does not need any params
+   */
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
